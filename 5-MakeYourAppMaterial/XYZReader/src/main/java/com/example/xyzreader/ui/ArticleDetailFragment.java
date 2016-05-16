88a110fecf4af36443b5d604ee69e30ba857f06a
Jeremy Silver <jeremy@udacity.com>
2016-05-16 16:11:49 -0700

    Switch to proper FAB

--- 5-MakeYourAppMaterial/XYZReader/src/main/java/com/example/xyzreader/ui/ArticleDetailFragment.java
+++ 5-MakeYourAppMaterial/XYZReader/src/main/java/com/example/xyzreader/ui/ArticleDetailFragment.java
@@ -8,7 +8,6 @@ import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.Rect;
-import android.graphics.Typeface;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v4.app.ShareCompat;
@@ -181,7 +180,6 @@ public class ArticleDetailFragment extends Fragment implements
         TextView bylineView = (TextView) mRootView.findViewById(R.id.article_byline);
         bylineView.setMovementMethod(new LinkMovementMethod());
         TextView bodyView = (TextView) mRootView.findViewById(R.id.article_body);
-        bodyView.setTypeface(Typeface.createFromAsset(getResources().getAssets(), "Rosario-Regular.ttf"));
 
         if (mCursor != null) {
             mRootView.setAlpha(0);
