2a6b0dd5e18c91a1b9dd197bd1206f885f73b3ac
Jeremy Silver <jeremy@udacity.com>
2016-07-15 13:00:54 -0700

    Fix bugs in Sunshine clone..

--- 6-GoUbiquitous/app/src/main/java/com/example/android/sunshine/app/Utility.java
+++ 6-GoUbiquitous/app/src/main/java/com/example/android/sunshine/app/Utility.java
@@ -91,9 +91,9 @@ public class Utility {
             String today = context.getString(R.string.today);
             int formatId = R.string.format_full_friendly_date;
             return String.format(context.getString(
-                    formatId,
+                    formatId),
                     today,
-                    getFormattedMonthDay(context, dateInMillis)));
+                    getFormattedMonthDay(context, dateInMillis));
         } else if (julianDay < currentJulianDay + 7) {
             // If the input date is less than a week in the future, just return the day name.
             return getDayName(context, dateInMillis);
@@ -117,9 +117,9 @@ public class Utility {
         String day = getDayName(context, dateInMillis);
         int formatId = R.string.format_full_friendly_date;
         return String.format(context.getString(
-                formatId,
+                formatId),
                 day,
-                getFormattedMonthDay(context, dateInMillis)));
+                getFormattedMonthDay(context, dateInMillis));
     }
 
     /**
